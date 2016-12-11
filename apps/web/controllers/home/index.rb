a6f183badc7ce37446f7bfb1fbe36f92431ce873
Jakub Pavlík <severus@post.cz>
2016-12-11 15:34:52 +0100

    homepage lists recently added/modified pieces

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -2,7 +2,12 @@ module Web::Controllers::Home
   class Index
     include Web::Action
 
+    expose :created_pieces, :updated_pieces
+
     def call(params)
+      repo = PieceRepository.new
+      @created_pieces = repo.newly_created
+      @updated_pieces = repo.newly_updated
     end
   end
 end
--- lib/db.inadiutorium/repositories/piece_repository.rb
+++ lib/db.inadiutorium/repositories/piece_repository.rb
@@ -1,2 +1,17 @@
 class PieceRepository < Hanami::Repository
+  def newly_created(limit: 10)
+    pieces
+      .order(:created_at)
+      .reverse
+      .limit(10)
+      .as(Piece)
+  end
+
+  def newly_updated(limit: 10)
+    pieces
+      .order(:updated_at)
+      .reverse
+      .limit(10)
+      .as(Piece)
+  end
 end
