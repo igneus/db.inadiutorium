4c3b67f2ffa6c65f1aea3bff7f1d7e9e9316bdb8
Jakub Pavlík <severus@post.cz>
2017-02-12 14:39:53 +0100

    pieces: pagination

--- apps/web/controllers/pieces/index.rb
+++ apps/web/controllers/pieces/index.rb
@@ -3,10 +3,18 @@ module Web::Controllers::Pieces
     include Web::Action
     include Inject['piece_repository']
 
-    expose :pieces
+    expose :page, :items, :pieces, :pages_total
+
+    params do
+      optional(:page).filled(:int?, gt?: 1)
+      optional(:items).filled(:int?, included_in?: 1..500)
+    end
 
     def call(params)
-      @pieces = piece_repository.all
+      @page = params[:page] || 1
+      @items = params[:items] || 20
+      @pieces = piece_repository.list(page: page, limit: items)
+      @pages_total = piece_repository.pages_total(limit: items)
     end
   end
 end
--- lib/db.inadiutorium/repositories/piece_repository.rb
+++ lib/db.inadiutorium/repositories/piece_repository.rb
@@ -3,11 +3,23 @@ class PieceRepository < Hanami::Repository
     pieces.count
   end
 
+  def list(limit: 10, page: 1)
+    pieces
+      .order(:lyrics_cleaned)
+      .limit(limit)
+      .offset(page - 1)
+      .as(Piece)
+  end
+
+  def pages_total(limit: 10)
+    (pieces.count.to_f / limit).ceil
+  end
+
   def newly_created(limit: 10)
     pieces
       .order(:created_at)
       .reverse
-      .limit(10)
+      .limit(limit)
       .as(Piece)
   end
 
@@ -15,7 +27,7 @@ class PieceRepository < Hanami::Repository
     pieces
       .order(:updated_at)
       .reverse
-      .limit(10)
+      .limit(limit)
       .as(Piece)
   end
 end
