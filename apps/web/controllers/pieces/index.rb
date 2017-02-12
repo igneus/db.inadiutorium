573423b8c0af8ce6490d907ecfc34ce87d041f10
Jakub Pavlík <severus@post.cz>
2017-02-12 15:27:00 +0100

    use variables defined locally
    not attribute methods defined by the framework

--- apps/web/controllers/pieces/index.rb
+++ apps/web/controllers/pieces/index.rb
@@ -13,8 +13,8 @@ module Web::Controllers::Pieces
     def call(params)
       @page = params[:page] || 1
       @items = params[:items] || 20
-      @pieces = piece_repository.list(page: page, limit: items)
-      @pages_total = piece_repository.pages_total(limit: items)
+      @pieces = piece_repository.list(page: @page, limit: @items)
+      @pages_total = piece_repository.pages_total(limit: @items)
     end
   end
 end
